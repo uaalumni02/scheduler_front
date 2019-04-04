a4102924688d2685ec0cfbe0a12ebbb09121d8f1
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-04-04 18:55:20 -0400

    updated server with babel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
@@ -12,5 +12,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/views'))
+
+app.get('/home', (req, res) => {
+    return res.render('home')
+});
 
 app.listen(port, () => console.log('frontend is live'));
