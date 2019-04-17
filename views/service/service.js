c53e7b36795d8ca205527ccf77faf972032c6d73
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-04-17 19:24:21 -0400

    updated views

--- views/service/service.js
+++ views/service/service.js
@@ -1,12 +1,19 @@
-const pageChange = () => {
+const nextButton1 = document.getElementById('serviceBtn');
+
+nextButton1.addEventListener('click', (e) => {
+    e.preventDefault();
     const selectedService = document.querySelector('input[name="service"]:checked').value;
     console.log(selectedService);
     document.getElementById('name').style.display = "none";
     document.getElementById('serviceBtn').style.display = "none";
     document.getElementById('calender').style.display = "block";
-    
-}
-const customerInfo = () => {
+
+})
+
+const nextButton2 = document.getElementById('calenderBtn');
+
+nextButton2.addEventListener('click', (e) => {
+    e.preventDefault();
     document.getElementById('calender').style.display = "none";
     document.getElementById('container-contact100').style.display = "block";
-}
+})
