bf2475e7676672d1b65740f4bb9f03aa54106e92
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-05-30 20:18:56 -0400

    import js

--- src/views/service/service.js
+++ src/views/service/service.js
@@ -1,13 +1,8 @@
 
-// const serviceSelector = () => {
-//     const selectedService = document.querySelector('input[name="service"]:checked').value;
-//     document.getElementById('name').style.display = "none";
-//     document.getElementById('calender').style.display = "block";
+
+
+// const customerInfo = () => {
+//     document.getElementById('calender').style.display = "none";
+//     document.getElementById('container-contact100').style.display = "block";
 // }
-
-
-const customerInfo = () => {
-    document.getElementById('calender').style.display = "none";
-    document.getElementById('container-contact100').style.display = "block";
-}
 
