a4bdc97551e143e4311ddffb9d97dd705548e277
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-05-30 20:21:54 -0400

    remove js file

--- src/views/service/service.js
+++
@@ -1,8 +0,0 @@
-
-
-
-// const customerInfo = () => {
-//     document.getElementById('calender').style.display = "none";
-//     document.getElementById('container-contact100').style.display = "block";
-// }
-
